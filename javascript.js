c0c9812cbe569680779362832022b4273d45e007
zippy-waluse <zippywaluse@gmail.com>
2024-04-05 20:56:09 +0300

    master

--- javascript.js
+++ javascript.js
@@ -1,28 +1,38 @@
-const api = '388e0feef41bd222a462713b50a45a7e';
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather'
+const api = '0876caa9edc6054c36bd00cced39b292';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
 
-const locationI = document.getElementById('location');
-const  searchB = document.getElementById('but1');
-const locationE = document.getElementById('PH');
-const temperature = document.getElementById('temp');
-const details = document.getElementById('details');
+
+     fetch(url)
+//     // JSON is often used when data is sent from a server to a web page
+//     .then(response => response.json())
+//
 
 
-// Code snippets -> small blocks of reusable code 
+const locationInput = document.getElementById('locationInput');
+const searchButton = document.getElementById('searchButton');
+const locationElement = document.getElementById('location');
+const temperatureElement = document.getElementById('temperature');
+const descriptionElement = document.getElementById('description');
 
-// The addEvent.... is an action that is initiated using the <input> element of type="search" .
-
-but1.addEventListener('click', () => {
-    const location = locationI.value;
-    if(location){
-        fetchweather(location);
+searchButton.addEventListener('click', () => {
+    const location = locationInput.value;
+    if (location) {
+        fetchWeather(location);
     }
-}
-)
+});
 
-function fetchweather(location){
-    // Query parameters are a defined set
-    //  of parameters (key-value pair) attached to the end of a URL used to provide additional information to a web server when making requests
-    const url = `${apiUrl}?q=${location}&appid=${apikey}`
+function fetchWeather(location) {
+    const url = `${apiUrl}?q=${location}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            locationElement.textContent = data.name;
+            temperatureElement.textContent = `${Math.round(data.main.temp)}°C`;
+            descriptionElement.textContent = data.weather[0].description;
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+        });
 }
